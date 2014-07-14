e6a8d44bf3fc1e436ea75422dedc769181e626a4
gembaf <gembaf.github@gmail.com>
2014-07-14 20:57:11 +0900

    Novelsコントローラファイルの作成 / テストデータの作成

---
+++ spec/factories/novels.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :novel do
+    title "notitle"
+    comment "hogehoge"
+    mode 0
+  end
+end
