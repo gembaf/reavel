f8e8948209e5ac30e04ba0fd266752d6e173685c
gembaf <gembaf.github@gmail.com>
2014-08-03 02:06:33 +0900

    閲覧ページでmodeによって間をスキップして表示できるように

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,33 +1,46 @@
 class MainsController < ApplicationController
+  def show
+  end
+
   def list
     @novels = Novel.active.updated_by
   end
 
-  def show
-  end
-
   def chapters_index
     @novel = Novel.where(id: params[:nid]).first
-    @models = Chapter.where(novel_id: params[:nid]).active.serial_by
-    render template: "mains/index"
+    @chapters = @novel.chapters.active.serial_by
   end
 
   def parts_index
     @novel = Novel.where(id: params[:nid]).first
-    @models = Part.where(chapter_id: params[:cid]).active.serial_by
-    render template: "mains/index"
+    @parts = Part.where(chapter_id: params[:cid]).active.serial_by
+
+    path = skip_path(@parts.first)
+    redirect_to path if path.present?
   end
 
   def volumes_index
     @novel = Novel.where(id: params[:nid]).first
-    @models = Volume.where(part_id: params[:pid]).active.serial_by
-    render template: "mains/index"
+    @volumes = Volume.where(part_id: params[:pid]).active.serial_by
   end
 
   def stories_index
     @novel = Novel.where(id: params[:nid]).first
-    @models = Story.where(volume_id: params[:vid]).active.serial_by
-    render template: "mains/index"
+    @stories = Story.where(volume_id: params[:vid]).active.serial_by
+  end
+
+  private
+  def skip_path(part)
+    return nil if part.nil?
+
+    volume = part.volumes.first
+    return nil if volume.nil?
+
+    if @novel.mode == Novel::MODE_MIDDLE
+      return mains_volumes_path(cid: params[:cid], pid: part.id)
+    elsif @novel.mode == Novel::MODE_SHORT
+      return mains_stories_path(pid: part.id, vid: volume.id)
+    end
   end
 end
 
