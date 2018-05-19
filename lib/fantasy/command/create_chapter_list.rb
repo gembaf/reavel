77a36ebbadf1e78b84f0922a0ee6438984182c2f
gembaf <gembaf.github@gmail.com>
2018-05-19 22:31:43 +0900

    チャプター名を切り出す処理を追加

--- lib/fantasy/command/create_chapter_list.rb
+++ lib/fantasy/command/create_chapter_list.rb
@@ -8,6 +8,18 @@ module Fantasy
       def execute
         'hoge'
       end
+
+      def story_title(line)
+        line.split(/　/).last.sub(%r{\(1/5\)}, '')
+      end
+
+      def small_chapter_title(line)
+        line.match(/・(.*?)編/)[1]
+      end
+
+      def big_chapter_title(line)
+        line.split(/：/).last
+      end
     end
   end
 end
--- spec/lib/fantasy/command/create_chapter_list_spec.rb
+++ spec/lib/fantasy/command/create_chapter_list_spec.rb
@@ -6,5 +6,59 @@ RSpec.describe Fantasy::Command::CreateChapterList do
   describe '#execute' do
     it { expect(instance.execute).to eq 'hoge' }
   end
+
+  describe '#story_title' do
+    subject { instance.story_title(line) }
+
+    context 'シンプルな文字列が来た場合' do
+      let(:line) { '第1話　発端(1/5)' }
+      it 'タイトルを抜き出せること'do
+        expect(subject).to eq '発端'
+      end
+    end
+
+    context '複雑な文字列が来た場合' do
+      let(:line) { '・ファンタジー俺　第四部　閑話31　とあるパーティの一日(1/5)' }
+      it 'タイトルを抜き出せること'do
+        expect(subject).to eq 'とあるパーティの一日'
+      end
+    end
+  end
+
+  describe '#small_chapter_title' do
+    subject { instance.small_chapter_title(line) }
+
+    context 'シンプルな文字列が来た場合' do
+      let(:line) { '・首なし騎士隊長編' }
+      it 'タイトルを抜き出せること'do
+        expect(subject).to eq '首なし騎士隊長'
+      end
+    end
+
+    context '複雑な文字列が来た場合' do
+      let(:line) { '・完全敗北、そして……編' }
+      it 'タイトルを抜き出せること'do
+        expect(subject).to eq '完全敗北、そして……'
+      end
+    end
+  end
+
+  describe '#big_chapter_title' do
+    subject { instance.big_chapter_title(line) }
+
+    context 'シンプルな文字列が来た場合' do
+      let(:line) { '●第一章：無垢なる少女' }
+      it 'タイトルを抜き出せること'do
+        expect(subject).to eq '無垢なる少女'
+      end
+    end
+
+    context '複雑な文字列が来た場合' do
+      let(:line) { '●第四部　第六章：魔王が来たりて笛を吹く' }
+      it 'タイトルを抜き出せること'do
+        expect(subject).to eq '魔王が来たりて笛を吹く'
+      end
+    end
+  end
 end
 
