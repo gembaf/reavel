fa31afe8ee31ba5e3a83a0caa68e0ace91b50513
gembaf <gembaf.github@gmail.com>
2018-05-20 15:42:24 +0900

    mhtからhtml部分だけを抜き出すクラスを追加

---
+++ lib/fantasy/command/scrape_mht.rb
@@ -0,0 +1,17 @@
+module Fantasy
+  module Command
+    class ScrapeMht
+      def self.execute
+        new.execute
+      end
+
+      def execute(path)
+        data = Reavel::Iconv.encode(File.read(path))
+        sections = data.split(/\r\n------=_NextPart.*?\r\n/)
+        target = sections.select { |sec| sec =~ %r{^Content-Type: text/html} }.first
+        base64_text = target.split(/\r\n\r\n/).last
+        base64_text.gsub(/\r\n/, '')
+      end
+    end
+  end
+end
