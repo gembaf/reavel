f5f1c23e95d775b109fc8f4866478e1f0f5557c7
gembaf <gembaf.github@gmail.com>
2018-05-20 18:01:59 +0900

    ファイルパスの確認

--- lib/fantasy/chapter_manager.rb
+++ lib/fantasy/chapter_manager.rb
@@ -2,13 +2,25 @@ require 'zip'
 
 module Fantasy
   class ChapterManager
+    IGNORE = %w(FORE_00.txt FORE_99_01.txt FORE_99_02.txt)
+
     attr_reader :dir_path
 
     def initialize(zip_file)
       @zip_file = zip_file
     end
 
+    def chapter_list_file
+      files.select { |name| name =~ /\.txt$/ }.first
+    end
+
+    def mht_files
+      files.select { |name| name =~ /\.mht$/ }
+    end
+
     def unzip
+      return if @dir_path.present?
+
       @dir_path = Rails.root.join('tmp', SecureRandom.uuid)
 
       ::Zip::File.open(@zip_file) do |zip|
@@ -18,6 +30,13 @@ module Fantasy
           zip.extract(entry, output) { true }
         end
       end
+    end
+
+    private
+
+    def files
+      unzip
+      `ls #{@dir_path}`.split(/\n/).reject { |name| IGNORE.include?(name) }
     end
   end
 end
--- spec/lib/fantasy/chapter_manager_spec.rb
+++ spec/lib/fantasy/chapter_manager_spec.rb
@@ -4,6 +4,22 @@ RSpec.describe Fantasy::ChapterManager do
   let(:instance) { described_class.new(zip_file) }
   let(:zip_file) { Rails.root.join('spec', 'fixtures', 'fantasy', 'chapter.zip') }
 
+  describe '#chapter_list_file' do
+    subject { instance.chapter_list_file }
+
+    it '正常に取得できること' do
+      expect(subject).to eq '第一部.txt'
+    end
+  end
+
+  describe '#mht_files' do
+    subject { instance.mht_files }
+
+    it '正常に取得できること' do
+      expect(subject.count).to eq 3
+    end
+  end
+
   describe '#unzip' do
     subject { instance.unzip }
 
