143dd2cafe10a3c0b077caeb27bc60ac51ecc8de
gembaf <gembaf.github@gmail.com>
2018-05-22 22:57:57 +0900

    rootパスを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   ##-- Viewer
+  root to: 'viewer#novels'
   get 'viewer' => 'viewer#novels'
   get 'viewer/:novel_id' => 'viewer#top_chapters', as: :viewer_top_chapters
   get 'viewer/:novel_id/:chapter_id' => 'viewer#chapters', as: :viewer_chapters
